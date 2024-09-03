f57fb8a4446785faf43603bbe1c9c2bbedb4e18f
Christopher Mejia <christophermejia111@gmail.com>
2024-09-03 19:28:16 -0400

    first commit

---
+++ token/token.go
@@ -0,0 +1,60 @@
+package token
+
+const (
+	// single character tokens
+	LEFT_PAREN = iota
+	RIGHT_PAREN
+	LEFT_BRACE
+	RIGHT_BRACE
+	COMMA
+	DOT
+	MINUS
+	PLUS
+	SEMICOLON
+	SLASH
+	STAR
+
+	// one or two character tokens
+	BANG
+	BANG_EQUAL
+	EQUAL
+	EQUAL_EQUAL
+	GREATER
+	GREATER_EQUAL
+	LESS
+	LESS_EQUAL
+
+	// literals
+	IDENTIFIER
+	STRING
+	NUMBER
+
+	// keywords
+	AND
+	CLASS
+	ELSE
+	FALSE
+	FUN
+	FOR
+	IF
+	NIL
+	OR
+	PRINT
+	RETURN
+	SUPER
+	THIS
+	TRUE
+	VAR
+	WHILE
+
+	EOF
+)
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Lexeme  string
+	Literal string
+	Line    int
+}
