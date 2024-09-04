7b2e0f03c16ea890d14db3d08cc569643613b2fc
Christopher Mejia <christophermejia111@gmail.com>
2024-09-03 21:45:25 -0400

    more tests for lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -29,7 +29,7 @@ func (l *Lexer) scanTokens() []token.Token {
 }
 
 func (l *Lexer) scanToken() {
-	ch := l.nextChar()
+	ch := l.advance()
 	switch ch {
 	case '(':
 		l.addToken(token.LEFT_PAREN, ch)
@@ -75,6 +75,19 @@ func (l *Lexer) scanToken() {
 		} else {
 			l.addToken(token.GREATER, ch)
 		}
+	case '/':
+		if l.match('/') {
+			for l.peek() != '\n' && !l.isAtEnd() {
+				l.advance()
+			}
+		} else {
+			l.addToken(token.SLASH, ch)
+		}
+	case ' ':
+	case '\r':
+	case '\t':
+	case '\n':
+		l.line += 1
 	default:
 		l.addToken(token.ILLEGAL, ch)
 	}
@@ -88,10 +101,17 @@ func (l *Lexer) isAtEnd() bool {
 	return l.current >= len(l.input)
 }
 
-func (l *Lexer) nextChar() byte {
+func (l *Lexer) advance() byte {
 	ch := l.input[l.current]
 	l.current += 1
 	return ch
+}
+
+func (l *Lexer) peek() byte {
+	if l.isAtEnd() {
+		return 0
+	}
+	return l.input[l.current]
 }
 
 func (l *Lexer) match(expected byte) bool {
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -7,17 +7,30 @@ import (
 
 func TestLexer(t *testing.T) {
 	t.Run("simple first test", func(t *testing.T) {
-		input := `{}*;.=!===`
+		input := `//
+		(( )){}
+		!*+-/=<> <= ==`
 
 		tests := []struct {
 			expectedType    token.TokenType
 			expectedLiteral string
 		}{
+			{token.LEFT_PAREN, "("},
+			{token.LEFT_PAREN, "("},
+			{token.RIGHT_PAREN, ")"},
+			{token.RIGHT_PAREN, ")"},
 			{token.LEFT_BRACE, "{"},
 			{token.RIGHT_BRACE, "}"},
+			{token.BANG, "!"},
 			{token.STAR, "*"},
-			{token.SEMICOLON, ";"},
-			{token.DOT, "."},
+			{token.PLUS, "+"},
+			{token.MINUS, "-"},
+			{token.SLASH, "/"},
+			{token.EQUAL, "="},
+			{token.LESS, "<"},
+			{token.GREATER, ">"},
+			{token.LESS_EQUAL, "<="},
+			{token.EQUAL_EQUAL, "=="},
 		}
 
 		l := New(input)
